42691fe873d5aaeb01ae9f40ab2785424923ac5a
BuildTools <unconfigured@null.spigotmc.org>
2022-11-28 16:25:46 +0700

    first commit

---
+++ overviewerConfig.js
@@ -0,0 +1,159 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "BDPSMP S3",
+        "BDPSMP S1",
+        "BDPSMP S2",
+        "BDPSMP S4"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1669564787",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "BDPSMP S3",
+            "zoomLevels": 14,
+            "defaultZoom": 1,
+            "maxZoom": 14,
+            "path": "normalrender1",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "BDPSMP S3",
+            "last_rendertime": 1631804846,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                80,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                80,
+                0
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "BDPSMP S1",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "normalrender2",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "BDPSMP S1",
+            "last_rendertime": 1633596373,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -240,
+                80,
+                -203
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                -240,
+                80,
+                -203
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "BDPSMP S2",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "normalrender3",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "BDPSMP S2",
+            "last_rendertime": 1631803892,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -32,
+                80,
+                43
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                -32,
+                80,
+                43
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "BDP SMP Final",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "normalrender4",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "BDPSMP S4",
+            "last_rendertime": 1631836505,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                128,
+                96,
+                176
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                128,
+                96,
+                176
+            ],
+            "north_direction": 2
+        }
+    ]
+};
